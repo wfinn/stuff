17c8fa3645d54fdd46b14aebbf15c1269c971dd3
Finn <fw@sernet.de>
2020-03-19 22:42:27 +0100

    Make message clearer (thx fervi)

--- downvote2donate/downvote2donate.go
+++ downvote2donate/downvote2donate.go
@@ -20,7 +20,7 @@ func main() {
 				for _, message := range messages.Data {
 					if strings.Contains(message.Message, "@downvote2donate") && message.Status == "Unread" {
 						if u, err := strconv.ParseUint(message.Link, 10, 32); err == nil {
-							if postresp, err := gozapread.SubmitNewPost("downvote2donate", `<h2>downvote2donate</h2>If you downvote this post <a href="https://github.com/Horndev/zapread.com#vote-examples">80% go to this group, 10% to the community and 10% to zapread</a>.`, gozapread.GetGroupId(uint(u))); err == nil {
+							if postresp, err := gozapread.SubmitNewPost("downvote2donate", `<h2>downvote2donate</h2>If you downvote this post <a href="https://github.com/Horndev/zapread.com#vote-examples">80% go to this group, 10% to the community and 10% to zapread</a>.<br>Mention me and I'll create this post in the group you mentioned me in.`, gozapread.GetGroupId(uint(u))); err == nil {
 							if commentId, err := strconv.ParseUint(message.Anchor, 10, 32); err == nil {
 								gozapread.AddComment(fmt.Sprintf(`<a href="https://www.zapread.com/Post/Detail/%d">Here</a>'s the post you can downvote to donate to this group.`, postresp.PostID), uint(u), uint(commentId))
 							}}
