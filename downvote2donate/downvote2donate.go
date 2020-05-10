24da8be9dce1747ca9bbd90d4896b24cb124d767
Finn <fw@sernet.de>
2020-05-10 20:13:42 +0200

    Fix mention detection

--- downvote2donate/downvote2donate.go
+++ downvote2donate/downvote2donate.go
@@ -21,7 +21,7 @@ func main() {
 		if api.UnreadMessages() {
 			if messages, err := api.GetMessageTable(); err == nil {
 				for _, message := range messages.Data {
-					if strings.Contains(message.Message, "@downvote2donate") && message.Status == "Unread" {
+					if strings.Contains(message.Message, "downvote2donate") && message.Status == "Unread" {
 						if postid, err := strconv.ParseUint(message.Link, 10, 32); err == nil {
 							if commentId, err := strconv.ParseUint(message.Anchor, 10, 32); err == nil {
 								api.AddComment(fmt.Sprintf(`Hi, %s!<br>You can <b>downvote this comment to donate to this group!</b><br><a href="https://github.com/Horndev/zapread.com#vote-examples">80%% of the donations go to this group, 10%% to the community and 10%% to zapread.</a>`, message.From), uint(postid), uint(commentId))
