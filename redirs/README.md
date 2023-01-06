866d27393b329c3171258ce844055371d4241eef
wfinn <no@mail>
2023-01-06 10:18:58 +0100

    restore redirs stuff

--- redirs/README.md
+++ redirs/README.md
@@ -1,6 +1,6 @@
 # redirs
 
-find redirects in a list of urls
+find (open) redirects in a list of urls
 
 ```sh
 # install
@@ -8,13 +8,12 @@ go install github.com/wfinn/stuff/redirs@latest
 
 # basic usage
 cat urls.txt | redirs
-
-# get some urls to scan
-gau target.tld | urlame > urls.txt
 ```
+
+This tool requests all urls, looks if somethin
 
 ## Flags
 
 - -r to set amount amount of go routines
 - -c session=abc123 to set a cookie
-- -a "Bearer: token" to se tteh Authorization header
+- -a "Bearer: token" to set the Authorization header
--- redirs/main.go
+++ redirs/main.go
@@ -10,7 +10,6 @@ import (
 	"net/url"
 	"os"
 	"regexp"
-	"strings"
 	"sync"
 	"time"
 )
@@ -18,9 +17,13 @@ import (
 var wwwregex = regexp.MustCompile("^www\\.")
 
 func main() {
-	routines := flag.Uint("r", 10, "go routines (concurrency)")
+	flag.Usage = func() {
+		fmt.Println("redirs - find (open) redirects automatically")
+		flag.PrintDefaults()
+	}
+	routines := flag.Uint("r", 1, "go routines (concurrency)")
 	cookies := flag.String("c", "", "cookies e.g. session=abc123")
-	authheader := flag.String("a", "", "Authorization header e.g. Bearer: abc123")
+	authheader := flag.String("a", "", "Authorization header e.g. Bearer: abc123") //verify := flag.String("verify", "", "activates scanning! supply attacker domain")
 	flag.Parse()
 	s := bufio.NewScanner(os.Stdin)
 	urls := make(chan string)
@@ -31,6 +34,7 @@ func main() {
 		go func() {
 			defer wg.Done()
 			for u := range urls {
+				fmt.Print(".")
 				if req, err := http.NewRequest("GET", u, nil); err == nil {
 					if *cookies != "" {
 						req.Header.Set("Cookie", *cookies)
@@ -41,10 +45,17 @@ func main() {
 					if resp, err := c.Do(req); err == nil {
 						if loc, err := resp.Location(); err == nil {
 							if uu, err := url.Parse(u); err == nil {
-								if isInterestingRedirect(uu, loc) {
-									fmt.Println("from:\t" + u)
-									fmt.Println("to:\t" + loc.String())
-									fmt.Println()
+								if !isLameRedirect(uu, loc) {
+									if commonsubstr := getMaxLengthCommonString(u, loc.String()); len(commonsubstr) < 3 {
+										continue
+									} else {
+										fmt.Println() //remove this
+										fmt.Println("from:\t" + u)
+										fmt.Println("to:\t" + loc.String())
+										fmt.Println("->\t" + commonsubstr)
+										fmt.Println()
+									}
+
 								}
 							}
 						}
@@ -60,58 +71,31 @@ func main() {
 	close(urls)
 }
 
-func isInterestingRedirect(f, t *url.URL) bool {
+func isLameRedirect(f, t *url.URL) bool {
 	fromhost := normalHost(f)
 	tohost := normalHost(t)
 	from := fromhost + normalPath(f) + normalQuery(f) + normalFragment(f)
 	to := tohost + normalPath(t) + normalQuery(t) + normalFragment(t)
 	if from == to {
 		//seems to be http to https or normalization
-		return false
+		return true
 	}
 	home := []string{"https://" + fromhost, "https://" + fromhost + "/"}
 	for _, loc := range home {
 		if loc == to {
 			// redirects to home are like 404
-			return false
+			return true
 		}
 	}
 	if f1, err := url.QueryUnescape(f.String()); err == nil {
 		if f2, err := url.QueryUnescape(t.String()); err == nil {
 			if f1 == f2 {
 				//ignore % normalization
-				return false
+				return true
 			}
 		}
 	}
-	return true
-}
-
-func normalHost(u *url.URL) string {
-	return strings.ToLower(wwwregex.ReplaceAllString(u.Hostname(), ""))
-}
-
-func normalPath(u *url.URL) string {
-	path := u.EscapedPath()
-	if path == "" {
-		return "/"
-	}
-	return path
-}
-
-func normalQuery(u *url.URL) string {
-	query := u.Query().Encode()
-	if query == "" {
-		return ""
-	}
-	return "?" + query
-}
-
-func normalFragment(u *url.URL) string {
-	if u.Fragment == "" {
-		return ""
-	}
-	return "#" + u.Fragment
+	return false
 }
 
 func getClient() *http.Client {
@@ -134,4 +118,41 @@ func getClient() *http.Client {
 		CheckRedirect: re,
 		Timeout:       time.Second * 10,
 	}
+}
+
+//https://www.codetd.com/en/article/13918456
+func getMaxLengthCommonString(str1, str2 string) string {
+	runes1 := []rune(str1)
+	runes2 := []rune(str2)
+	chs1 := len(runes1)
+	chs2 := len(runes2)
+
+	maxLength := 0 //Record the maximum length
+	end := 0       //The end position of the record maximum length
+
+	rows := 0
+	cols := chs2 - 1
+	for rows < chs1 {
+		i, j := rows, cols
+		length := 0 //record length
+		for i < chs1 && j < chs2 {
+			if runes1[i] != runes2[j] {
+				length = 0
+			} else {
+				length++
+			}
+			if length > maxLength {
+				end = i
+				maxLength = length
+			}
+			i++
+			j++
+		}
+		if cols > 0 {
+			cols--
+		} else {
+			rows++
+		}
+	}
+	return string(runes1[(end - maxLength + 1):(end + 1)])
 }
